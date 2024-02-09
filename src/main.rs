e6523cfe9c19267b80e47e727265596a5e492ea5
dayshah <dhyeyhshah@gmail.com>
2024-02-09 00:23:03 -0500

    bytes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+use mini_redis::Command::*;
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
 
@@ -6,16 +8,28 @@ async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
     loop {
         let (socket, _) = listener.accept().await.unwrap();
-        process(socket).await;
+        tokio::spawn(async move { process(socket) });
     }
 }
 
 async fn process(socket: TcpStream) {
+    let mut db = HashMap::new();
     let mut connection = Connection::new(socket);
     if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
-
-        let response = Frame::Error("unimplemented".to_owned());
+        let response = match mini_redis::Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_owned(), cmd.value().to_owned());
+                Frame::Simple("Ok".to_owned())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("still gotta get to it {:?}", cmd)
+        };
         connection.write_frame(&response).await.unwrap();
     }
 }
